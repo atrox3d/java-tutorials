7ec02df08fed0c8a9992dfb2757b40c9091102ec
atrox3d <atrox3d@gmail.com>
2016-03-04 00:37:26 +0100

    add abstract and actual computers

---
+++ design-patterns-factory/src/main/java/io/github/ingloriouscoderz/AbstractComputer.java
@@ -0,0 +1,18 @@
+package io.github.ingloriouscoderz;
+
+public abstract class AbstractComputer {
+	
+	public abstract String getRAM();
+	public abstract String getHDD();
+	public abstract String getCPU();
+
+	@Override
+	public String toString() {
+		return String.format(
+					"RAM=%s, HDD=%s, CPU=%s",
+					getRAM(),
+					getHDD(),
+					getCPU()
+				);
+	}
+}
