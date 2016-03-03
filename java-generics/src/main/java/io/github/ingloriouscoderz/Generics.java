ec26a018dfc2cff9d71d25363f7fe456b1bf2aec
atrox3d <atrox3d@gmail.com>
2016-03-03 17:18:55 +0100

    end generics

--- java-generics/src/main/java/io/github/ingloriouscoderz/Generics.java
+++ java-generics/src/main/java/io/github/ingloriouscoderz/Generics.java
@@ -17,6 +17,8 @@ public class Generics {
 
 		GenericBox<String> gbs = new GenericBox<String>("sette");
 		System.out.println(gbs);
+		
+		System.out.println("test Class<E>clazz==E value.class: " + testClass(Integer.class, 5));
 	}
 
 	public static <E> void printVar(E var) {
@@ -28,4 +30,11 @@ public class Generics {
 		System.out.println("returning [" + clazz.getName() + "] with value : " + value);
 		return var;
 	}
+	
+	public static <E> boolean testClass(Class<E> clazz, E value) {
+		return value.getClass() == clazz;
+		
+	}
+	
+	
 }
