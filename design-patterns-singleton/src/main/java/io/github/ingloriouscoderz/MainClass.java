24cb79bd1333f9e3c58755646bc2a632f0a7e879
atrox3d <atrox3d@gmail.com>
2016-03-04 00:04:37 +0100

    add hashCode method

--- design-patterns-singleton/src/main/java/io/github/ingloriouscoderz/MainClass.java
+++ design-patterns-singleton/src/main/java/io/github/ingloriouscoderz/MainClass.java
@@ -7,30 +7,40 @@ public class MainClass {
 		System.out.println("testing EagerInitializedSingleton...");
 		EagerInitializedSingleton eis1 = EagerInitializedSingleton.getInstance();
 		EagerInitializedSingleton eis2 = EagerInitializedSingleton.getInstance();
+		System.out.println("instance1 hashCode=" + eis1.hashCode());
+		System.out.println("instance2 hashCode=" + eis2.hashCode());
 		System.out.println("instance1==instance2:" + (eis1==eis2));
 		
 		System.out.println("-----------------------------------------------------------------------");
 		System.out.println("testing StaticBlockSingleton...");
 		StaticBlockSingleton sbs1 = StaticBlockSingleton.getInstance();
 		StaticBlockSingleton sbs2 = StaticBlockSingleton.getInstance();
+		System.out.println("instance1 hashCode=" + sbs1.hashCode());
+		System.out.println("instance2 hashCode=" + sbs2.hashCode());
 		System.out.println("instance1==instance2:" + (sbs1==sbs2));
 		
 		System.out.println("-----------------------------------------------------------------------");
 		System.out.println("testing LazyInitializedSingleton...");
 		LazyInitializedSingleton lis1 = LazyInitializedSingleton.getInstance();
 		LazyInitializedSingleton lis2 = LazyInitializedSingleton.getInstance();
+		System.out.println("instance1 hashCode=" + lis1.hashCode());
+		System.out.println("instance2 hashCode=" + lis2.hashCode());
 		System.out.println("instance1==instance2:" + (lis1==lis2));
 
 		System.out.println("-----------------------------------------------------------------------");
 		System.out.println("testing ThreadSafeSingleton...");
 		ThreadSafeSingleton tss1 = ThreadSafeSingleton.getInstance();
 		ThreadSafeSingleton tss2 = ThreadSafeSingleton.getInstance();
+		System.out.println("instance1 hashCode=" + tss1.hashCode());
+		System.out.println("instance2 hashCode=" + tss2.hashCode());
 		System.out.println("instance1==instance2:" + (tss1==tss2));
 
 		System.out.println("-----------------------------------------------------------------------");
 		System.out.println("testing InnerClassSingleton...");
 		InnerClassSingleton ics1 = InnerClassSingleton.getInstance();
 		InnerClassSingleton ics2 = InnerClassSingleton.getInstance();
+		System.out.println("instance1 hashCode=" + ics1.hashCode());
+		System.out.println("instance2 hashCode=" + ics2.hashCode());
 		System.out.println("instance1==instance2:" + (ics1==ics2));
 }
 
