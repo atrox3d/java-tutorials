b76d0a80d4a6afd0c338d3465143012cf549c13d
atrox3d <atrox3d@gmail.com>
2016-03-05 22:22:41 +0100

    add proxy

---
+++ design-patterns-proxy/src/main/java/model/Employee.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Employee {
+	int id;
+	String name;
+	public Employee() {
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
