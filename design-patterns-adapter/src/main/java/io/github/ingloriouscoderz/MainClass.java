3fd39e98f9c00e80f513b7079510049439609ce0
atrox3d <atrox3d@gmail.com>
2016-03-07 11:01:38 +0100

    add main tester

--- design-patterns-adapter/src/main/java/io/github/ingloriouscoderz/MainClass.java
+++ design-patterns-adapter/src/main/java/io/github/ingloriouscoderz/MainClass.java
@@ -1,9 +1,34 @@
 package io.github.ingloriouscoderz;
 
+import io.github.ingloriouscoderz.design.adapter.SocketAdapter;
+import io.github.ingloriouscoderz.design.adapter.SocketClassAdapter;
+import io.github.ingloriouscoderz.design.adapter.SocketObjectAdapter;
+import io.github.ingloriouscoderz.design.adapter.Volt;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 	}
+	
+	public void testClassAdapter() {
+		SocketAdapter sockAdapter = new SocketClassAdapter();
+		Volt v3 = sockAdapter.get3Volt();
+		Volt v12 = sockAdapter.get12Volt();
+		Volt v120= sockAdapter.get120Volt();
+		
+		System.out.println("v3 using class adapter=" + v3.getVolts());
+		System.out.println("v12 using class adapter=" + v12.getVolts());
+		System.out.println("v120 using class adapter=" + v120.getVolts());
+	}
 
+	public void testObjectAdapter() {
+		SocketAdapter sockAdapter = new SocketObjectAdapter();
+		Volt v3 = sockAdapter.get3Volt();
+		Volt v12 = sockAdapter.get12Volt();
+		Volt v120= sockAdapter.get120Volt();
+		
+		System.out.println("v3 using object adapter=" + v3.getVolts());
+		System.out.println("v12 using object adapter=" + v12.getVolts());
+		System.out.println("v120 using object adapter=" + v120.getVolts());
+	}
 }
