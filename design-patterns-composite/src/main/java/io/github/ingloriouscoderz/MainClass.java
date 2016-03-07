8466e91e52b8462a01b957b5f13b7e3df4b2b1a0
atrox3d <atrox3d@gmail.com>
2016-03-07 13:05:51 +0100

    add main method

--- design-patterns-composite/src/main/java/io/github/ingloriouscoderz/MainClass.java
+++ design-patterns-composite/src/main/java/io/github/ingloriouscoderz/MainClass.java
@@ -1,6 +1,24 @@
 package io.github.ingloriouscoderz;
 
+import io.github.ingloriouscoderz.design.composite.Circle;
+import io.github.ingloriouscoderz.design.composite.Drawing;
+import io.github.ingloriouscoderz.design.composite.Triangle;
 
 public class MainClass {
 
+	public static void main(String[] args) {
+		Drawing d = new Drawing();
+		
+		for(int i=0; i<3; i++) {
+			d.add(new Triangle());
+			d.add(new Circle());
+		}
+		
+		d.draw("red");
+		
+		d.clear();
+		d.add(new Triangle());
+		d.add(new Circle());
+		d.draw("black");
+	}
 }
