5283a85eb096c18c9bd46e4f61d6f6d0933dd533
atrox3d <atrox3d@gmail.com>
2016-03-10 10:44:28 +0100

    add socket-client.js

--- load-balancer-emulator/src/main/java/io/github/ingloriouscoderz/MainClass.java
+++ load-balancer-emulator/src/main/java/io/github/ingloriouscoderz/MainClass.java
@@ -3,18 +3,19 @@ package io.github.ingloriouscoderz;
 import io.github.ingloriouscoderz.network.client.PseudoPingEcho;
 import io.github.ingloriouscoderz.network.client.ReachableTest;
 import io.github.ingloriouscoderz.network.client.SocketClient;
+import io.github.ingloriouscoderz.network.server.SocketServer;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		PseudoPingEcho ppe = new PseudoPingEcho("127.0.0.1", 7);
-		ppe.ping(5000);
+//		PseudoPingEcho ppe = new PseudoPingEcho("127.0.0.1", 7);
+//		ppe.ping(5000);
 		
 //		ReachableTest rt = new ReachableTest("127.0.0.1");
 //		rt.ping(3000);
 		
 //		SocketClient sc = new SocketClient("127.0.0.1", 7);
 //		sc.ping(3000);
+		new SocketServer(7, 10).serve();
 	}
-
 }
